41d2262ea0d91cad18a24b74041c882ffadd4a10
root <kmahlatsi64@gmail.com>
2023-03-21 10:50:24 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_Putchar('_');
+	_Putchar('p');
+	_Putchar('u');
+	_Putchar('t');
+	_Putchar('c');
+	_Putchar('h');
+	_Putchar('a');
+	_Putchar('r');
+	_Putchar('\n');
+	return (0);
+}
