49ffeaa4858cbe0430a921eaeb5115c02e49c8ce
root <kmahlatsi64@gmail.com>
2023-04-05 11:21:49 -0700

    sss

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,14 @@
  */
 
 void _puts_recursion(char *s)
-
 {
 	if (*s)
 	{
-		_putchar(*s)
-		_put_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
 	else
 		_putchar('\n');
-
 }
+
